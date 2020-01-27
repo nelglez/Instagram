6344eb11639c8d91d55ba9c152b3452283df086e
Nelson Gonzalez <nelglezfl@gmail.com>
2020-01-27 12:01:06 -0500

    Add tab view.

---
+++ Instagram/Components/HomeView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeView.swift
+//  Instagram
+//
+//  Created by Nelson Gonzalez on 1/27/20.
+//  Copyright © 2020 Nelson Gonzalez. All rights reserved.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
--- Instagram/Components/MainView.swift
+++ Instagram/Components/MainView.swift
@@ -11,12 +11,29 @@ import SwiftUI
 struct MainView: View {
     @EnvironmentObject var session: SessionStore
     var body: some View {
-        Group {
-            Text((session.userSession == nil) ? "Loading..." : session.userSession!.email)
-            Button(action: logOut) {
-                Text("Log out")
-            }
-        }
+        TabView {
+            HomeView().tabItem {
+                Image(systemName: "house.fill")
+            }.tag(0)
+            Text("Search View").tabItem({
+                Image(systemName: "magnifyingglass")
+            }).tag(1)
+            Text("Camera View").tabItem({
+                Image(systemName: "camera.viewfinder")
+            }).tag(2)
+            Text("Notification View").tabItem({
+                Image(systemName: "heart.fill")
+            }).tag(3)
+            Text("Profile View").tabItem({
+                Image(systemName: "person.fill")
+            }).tag(4)
+        }.accentColor(.black)
+//        Group {
+//            Text((session.userSession == nil) ? "Loading..." : session.userSession!.email)
+//            Button(action: logOut) {
+//                Text("Log out")
+//            }
+//        }
     }
     
     private func logOut() {
