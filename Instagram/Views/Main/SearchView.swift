cfa2df24cfea6fa4ee3565eef67f8c77e9a346b7
Nelson Gonzalez <nelglezfl@gmail.com>
2020-02-02 10:38:46 -0500

    Discovery View finish up for now with mock data.

--- Instagram/Views/Main/SearchView.swift
+++ Instagram/Views/Main/SearchView.swift
@@ -15,18 +15,30 @@ struct Photo: Identifiable {
 
 struct SearchView: View {
     var photoArray = [Photo(photo: "photo"), Photo(photo: "photo1"), Photo(photo: "photo2"), Photo(photo: "photo3"), Photo(photo: "photo4"), Photo(photo: "photo5"), Photo(photo: "photo6"), Photo(photo: "photo7"), Photo(photo: "photo8"), Photo(photo: "photo9")]
+    @State private var selection = 0
+    var displayState = ["square.grid.2x2.fill", "list.dash"]
+    
     var body: some View {
         let splittedArray = photoArray.splited(into: 3)
-       return ScrollView {
-            //Rows
-            ForEach(0..<splittedArray.count) { index in
-                HStack {
-                    //Columns
-                    ForEach(splittedArray[index]) { photo_element in
-                        Image(photo_element.photo).resizable().scaledToFit()
+        return
+            NavigationView {
+                ScrollView {
+                    Picker(selection: $selection, label: Text("Grid or Table")) {
+                        ForEach(0..<displayState.count) { index in
+                            
+                            Image(self.displayState[index]).tag(index)
+                        }
+                    }.pickerStyle(SegmentedPickerStyle()).padding()
+                    //Rows
+                    ForEach(0..<splittedArray.count) { index in
+                        HStack {
+                            //Columns
+                            ForEach(splittedArray[index]) { photo_element in
+                                Image(photo_element.photo).resizable().scaledToFill().frame(width: UIScreen.main.bounds.width / 2, height: UIScreen.main.bounds.width / 2).clipped()//Prevents image to overlap its boundaries.
+                            }
+                        }
                     }
-                }
-            }
+                }.navigationBarTitle(Text("Discover"), displayMode: .inline)
         }
     }
 }
